090533eb35bcc20b5f7c30204e1df31514a064df
Enzo Barrett <enzobarrett@gmail.com>
2019-11-01 22:51:21 -0600

    update js

--- displayUnix.js
+++ displayUnix.js
@@ -1,22 +1,20 @@
-var displayed = 0;
 document.getElementById("unixmover").style.display = "none";
-setInterval(mousemov, 20);
 display();
 document.onmousemove = function(e) {
     var event = e || window.event;
     window.mouseX = event.clientX;
     window.mouseY = event.clientY;
-    displayed = 1;
+    document.getElementById("unixmover").style.left = (window.mouseX + 10) + "px";
+    document.getElementById("unixmover").style.top = (window.mouseY - 10 + window.scrollY) + "px";
+    document.getElementById("unixmover").style.display = "inline";
+}
+window.onscroll = function scrollMove() {
+    document.getElementById("unixmover").style.left = (window.mouseX + 10) + "px";
+    document.getElementById("unixmover").style.top = (window.mouseY - 10 + window.scrollY) + "px";
+    document.getElementById("unixmover").style.display = "inline";
 }
 function display() {
     document.getElementsByClassName("unix")[0].innerHTML = Math.round(new Date().getTime()/1000);
     document.getElementsByClassName("unix")[1].innerHTML = Math.round(new Date().getTime()/1000);
     window.setTimeout(display, 1000);
-}
-function mousemov() {
-    document.getElementById("unixmover").style.left = (window.mouseX + 10) + "px";
-    document.getElementById("unixmover").style.top = (window.mouseY - 10 + window.scrollY) + "px";
-    if (displayed) {
-	document.getElementById("unixmover").style.display = "inline";
-    }
 }
