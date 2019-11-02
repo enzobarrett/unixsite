755ceda6791bdc83e283c4b484e9ce23c3c302ab
Enzo Barrett <enzobarrett@gmail.com>
2019-11-01 22:36:41 -0600

    fix mouse changer and hide prices

--- displayUnix.js
+++ displayUnix.js
@@ -1,9 +1,12 @@
-setInterval(mousemov, 10);
+var displayed = 0;
+document.getElementById("unixmover").style.display = "none";
+setInterval(mousemov, 20);
 display();
 document.onmousemove = function(e) {
     var event = e || window.event;
     window.mouseX = event.clientX;
     window.mouseY = event.clientY;
+    displayed = 1;
 }
 function display() {
     document.getElementsByClassName("unix")[0].innerHTML = Math.round(new Date().getTime()/1000);
@@ -13,4 +16,7 @@ function display() {
 function mousemov() {
     document.getElementById("unixmover").style.left = (window.mouseX + 10) + "px";
     document.getElementById("unixmover").style.top = (window.mouseY - 10 + window.scrollY) + "px";
+    if (displayed) {
+	document.getElementById("unixmover").style.display = "inline";
+    }
 }
